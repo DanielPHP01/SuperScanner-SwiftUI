3facda049017c47fc8e8d72e5987a5d8b08ed3f7
Daniel <97878253+DanielPHP01@users.noreply.github.com>
2024-06-26 19:11:50 +0600

    Fix whitespace issue in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,26 +1,30 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SuperScanner-SwiftUI",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14)
     ],
     products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SuperScanner-SwiftUI",
-            targets: ["SuperScanner-SwiftUI"]
-        ),
+            targets: ["SuperScanner-SwiftUI"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SuperScanner-SwiftUI",
-            dependencies: [],
-            path: "Sources"
-        )
+            dependencies: [])
     ],
     swiftLanguageVersions: [
-        .v5
+        .version("5.5")
     ]
 )
